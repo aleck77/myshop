586dfc89c4a636bd31adbf1612887afa1b7b089d
aleck77 <a.oshatjuk@gmail.com>
2016-10-29 22:04:13 +0300

    BooleanField

--- shop/admin.py
+++ shop/admin.py
@@ -1,7 +1,21 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 from models import Category, Product
 
-admin.site.register(Category)
-admin.site.register(Product)
+# Модель категории
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name', )}
+
+# Модель товара
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'stock', 'available', 'created', 'updated']
+    list_filter = ['available', 'created', 'updated']
+    list_editable = ['price', 'stock', 'available']
+    prepopulated_fields = {'slug': ('name', )}
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
 
 # Register your models here.
--- shop/models.py
+++ shop/models.py
@@ -1,12 +1,12 @@
-#! /usr/bin/env python
+#! /usr/bin/python
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
+# from __future__ import unicode_literals
 from django.db import models
+from django.core.urlresolvers import reverse
 
 # Модель категории
 class Category(models.Model):
-    name = models.CharField(max_length=200, db_index=True)
+    name = models.CharField(max_length=200, db_index=True, verbose_name='Имя')
     slug = models.SlugField(max_length=200, db_index=True, unique=True)
 
     class Meta:
@@ -17,6 +17,8 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return reverse('shop:ProductListByCategory', args=[self.slug])
 
 # Модель продукта
 class Product(models.Model):
@@ -27,9 +29,9 @@ class Product(models.Model):
     description = models.TextField(blank=True, verbose_name="Описание")
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
     stock = models.PositiveIntegerField(verbose_name="На складе")
-    available = models.BinaryField(default=True, verbose_name="Доступен")
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    available = models.BooleanField(default=True, verbose_name="Доступен")
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Создан')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Обновлен')
 
     class Meta:
         ordering = ['name']
@@ -41,3 +43,6 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:ProductDetail', args=[self.id, self.slug])
